0f76606dded627894224e085f5f68404b6648721
josd <josderoo@gmail.com>
2024-11-13 15:49:39 +0100

    refreshing

--- examples/fourcolor.pl
+++ examples/fourcolor.pl
@@ -10,44 +10,44 @@ places([]).
 places([[Place,Color]|Tail]) :-
     places(Tail),
     'urn:example:neighbours'(Place,Neighbours),
-    member(Color,['urn:example:c1','urn:example:c2','urn:example:c3','urn:example:c4']),
+    member(Color,['urn:example:red','urn:example:green','urn:example:blue','urn:example:yellow']),
     \+ (member([Neighbour,Color],Tail),member(Neighbour,Neighbours)).
 
 % test data
 'urn:example:neighbours'(
-    'urn:example:p1',
-    [   'urn:example:p2',
-        'urn:example:p5',
-        'urn:example:p4',
-        'urn:example:p3'
+    'urn:example:place1',
+    [   'urn:example:place2',
+        'urn:example:place5',
+        'urn:example:place4',
+        'urn:example:place3'
     ]
 ).
 'urn:example:neighbours'(
-    'urn:example:p2',
-    [   'urn:example:p1',
-        'urn:example:p4',
-        'urn:example:p3'
+    'urn:example:place2',
+    [   'urn:example:place1',
+        'urn:example:place4',
+        'urn:example:place3'
     ]
 ).
 'urn:example:neighbours'(
-    'urn:example:p3',
-    [   'urn:example:p5',
-        'urn:example:p1',
-        'urn:example:p4',
-        'urn:example:p2'
+    'urn:example:place3',
+    [   'urn:example:place5',
+        'urn:example:place1',
+        'urn:example:place4',
+        'urn:example:place2'
     ]
 ).
 'urn:example:neighbours'(
-    'urn:example:p4',
-    [   'urn:example:p1',
-        'urn:example:p2',
-        'urn:example:p3'
+    'urn:example:place4',
+    [   'urn:example:place1',
+        'urn:example:place2',
+        'urn:example:place3'
     ]
 ).
 'urn:example:neighbours'(
-    'urn:example:p5',
-    [   'urn:example:p1',
-        'urn:example:p3'
+    'urn:example:place5',
+    [   'urn:example:place1',
+        'urn:example:place3'
     ]
 ).
 

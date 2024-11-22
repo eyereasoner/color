71d7f7b5e96166f4ad0785c2a94f7da873aa2a3e
josd <josderoo@gmail.com>
2024-11-22 16:04:08 +0100

    WIP

--- etc/workplace-benchmark.pl
+++ etc/workplace-benchmark.pl
@@ -5,9 +5,10 @@
 :- dynamic('urn:example:does'/2).
 :- dynamic('urn:example:complies'/2).
 
-% Facts about what 1000 employees are doing
-prepare :-
-    between(1,10000,N),
+% Facts about what employees are doing
+'urn:example:prepare'(X,Y) <=
+    Z is Y//3,
+    between(X,Z,N),
     number_codes(N,J),
     atom_codes(I,J),
     atom_concat('urn:example:alice',I,A),
@@ -27,4 +28,5 @@ prepare :-
 'urn:example:does'(Person,'urn:example:access_social_media') => 'urn:example:complies'(Person,false).
 
 % Query to test if everyone complies with deontic logic
-prepare,'urn:example:complies'(_Person,_Check) => true.
+'urn:example:prepare'(1,30000) => true.
+'urn:example:complies'(_Person,_Check) => true.

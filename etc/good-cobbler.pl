c3405f2196401b6592b196251ea12d6c81025419
josd <josderoo@gmail.com>
2024-12-01 23:23:42 +0100

    refreshing

--- etc/good-cobbler.pl
+++ etc/good-cobbler.pl
@@ -8,4 +8,5 @@
 'urn:example:is'(_, 'urn:example:good'('urn:example:Cobbler')) ::- true.
 
 % is there some x which is good at some y
-true ::- ('urn:example:is'(_, 'urn:example:good'(_)) ::- true).
+true ::-
+    ('urn:example:is'(_, 'urn:example:good'(_)) ::- true).

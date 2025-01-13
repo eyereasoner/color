50ef049f5a08a033c4bfc809ca3c3e8bc85083f9
josd <josderoo@gmail.com>
2025-01-13 16:16:10 +0100

    refreshing

--- runs/age-zkp.pl
+++ runs/age-zkp.pl
@@ -27,7 +27,7 @@
     'urn:example:personAge'(Simulation, Name, PersonAge),
     'urn:example:age'(Simulation, SimulationAge),
     write_term_to_chars(PersonAge > SimulationAge, [], Chars),
-    crypto_data_hash(Chars, Hash, [algorithm(sha256)]),
+    crypto_data_hash(Chars, Hash, [algorithm(blake2b512)]),
     (   PersonAge > SimulationAge
     ->  Result = "proof verified: more than 50 years old and entitled to work 80% per week"
     ;   Result = "proof failed: less than 50 years and not entitled to work 80% per week"
@@ -38,7 +38,7 @@
     'urn:example:personAge'(Simulation, Name, PersonAge),
     'urn:example:age'(Simulation, SimulationAge),
     write_term_to_chars(PersonAge > SimulationAge, [], Chars),
-    crypto_data_hash(Chars, ProofHash, [algorithm(sha256)]),
+    crypto_data_hash(Chars, ProofHash, [algorithm(blake2b512)]),
     (   Hash = ProofHash
     ->  Result = "challenge successful: the proof is valid"
     ;   Result = "challenge failed: the proof is invalid"
@@ -47,8 +47,8 @@
 % queries
 query('urn:example:zeroKnowledgeProof'('urn:example:simulation1', 'urn:example:Mary', _, _)).
 query('urn:example:zeroKnowledgeProof'('urn:example:simulation1', 'urn:example:Pat', _, _)).
-query('urn:example:challengeProof'('urn:example:simulation1', 'urn:example:Mary', _, "94c9923c71b5dac8e5a8f409e3d5d285f7b0ca7d08fe7529a112103188726c75")).
-query('urn:example:challengeProof'('urn:example:simulation1', 'urn:example:Pat', _, "66b486e614df3fcdb4ea91faa853fad3a50f170271254894bab22a234f430bee")).
+query('urn:example:challengeProof'('urn:example:simulation1', 'urn:example:Mary', _, "d8a01900f6ab30e14dbce67939c46a29d4ade0f8aec11801d1c556a452e7653d6c1ea89b4d8643cde78398dbee87d90890406dc8a8b92bbbe94f09c3ea3b0ffd")).
+query('urn:example:challengeProof'('urn:example:simulation1', 'urn:example:Pat', _, "6b5e3584bbe589413f83c584ba064275a3fdf867aaa66acf25b14c6275439c90b0b0c7afafd478fc8feec73ae355f5cd8560264f66e9ffd118ae8577ffb81e13")).
 
 run :-
     query(Q),

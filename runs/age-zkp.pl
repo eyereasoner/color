8a644aea2a8f957e6a3c5b17b80e5d85dadd8eae
josd <josderoo@gmail.com>
2025-01-14 12:01:55 +0100

    refreshing

--- runs/age-zkp.pl
+++ runs/age-zkp.pl
@@ -33,9 +33,22 @@
     ;   Result = "proof failed: less than 50 years and not entitled to work 80% per week"
     ).
 
+% verifier checking the proof using the hash
+'urn:example:challengeProof'(Simulation, [Name, Result, Hash]) :-
+    'urn:example:personAge'(Simulation, [Name, PersonAge]),
+    'urn:example:age'(Simulation, SimulationAge),
+    write_term_to_chars(PersonAge > SimulationAge, [], Chars),
+    crypto_data_hash(Chars, ProofHash, [algorithm(sha3_512)]),
+    (   Hash = ProofHash
+    ->  Result = "challenge successful: the proof is valid"
+    ;   Result = "challenge failed: the proof is invalid"
+    ).
+
 % queries
 query('urn:example:zeroKnowledgeProof'('urn:example:simulation1', ['urn:example:Mary', _, _])).
 query('urn:example:zeroKnowledgeProof'('urn:example:simulation1', ['urn:example:Pat', _, _])).
+query('urn:example:challengeProof'('urn:example:simulation1', ['urn:example:Mary', _, "6151e3412bc493063a9be0aadd3ecaaae9ebdca0f5d65441192fd14660f1ef91918cd29181414d8b48ade139d11d33156618878df03cdaab9e9296370b00e821"])).
+query('urn:example:challengeProof'('urn:example:simulation1', ['urn:example:Pat', _, "4ac8666fbe3e7a9206d453134ca6c38d8fa740b1e589a73c2a767c3679ac682c6e5919434a931e8b37ef62ed0ad942dab41c61e7960061dc1715bc10c0e525a8"])).
 
 run :-
     query(Q),

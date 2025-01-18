075132e953e5ad165ebf4f26864cae605b5beb68
josd <josderoo@gmail.com>
2025-01-18 23:27:02 +0100

    refreshing

---
+++ etc/output/proof-by-contrapositive.pl
@@ -0,0 +1,7 @@
+:- op(1200, xfx, :+).
+
+answer((false:+'urn:example:is'('urn:example:ground', 'urn:example:wet'))).
+answer((false:+'urn:example:is'('urn:example:it', 'urn:example:raining'))).
+
+% proof steps
+step(((false:+A):+(B:+A), (false:+B)), (('urn:example:is'('urn:example:ground', 'urn:example:wet'):+'urn:example:is'('urn:example:it', 'urn:example:raining')), (false:+'urn:example:is'('urn:example:ground', 'urn:example:wet'))), (false:+'urn:example:is'('urn:example:it', 'urn:example:raining'))).
---
+++ etc/proof-by-cases.pl
@@ -0,0 +1,38 @@
+% Proof by cases
+% See https://en.wikipedia.org/wiki/Disjunction_elimination
+
+:- dynamic('urn:example:allPossibleCases'/2).
+:- dynamic('urn:example:is'/2).
+
+% water is an inorganic compound
+'urn:example:InorganicCompound'('urn:example:water').
+
+% water is solid or liquid or gas
+'urn:example:allPossibleCases'([A],
+        [
+            'urn:example:is'(A, 'urn:example:solid'),
+            'urn:example:is'(A, 'urn:example:liquid'),
+            'urn:example:is'(A, 'urn:example:gas')
+        ]
+    ) :+ 'urn:example:InorganicCompound'(A).
+
+% solid, liquid and gas things are observable
+'urn:example:is'(A, 'urn:example:observable') :+
+    'urn:example:is'(A, 'urn:example:solid').
+
+'urn:example:is'(A, 'urn:example:observable') :+
+    'urn:example:is'(A, 'urn:example:liquid').
+
+'urn:example:is'(A, 'urn:example:observable') :+
+    'urn:example:is'(A, 'urn:example:gas').
+
+% proof by cases
+'urn:example:is'(A, 'urn:example:observable') :+
+    'urn:example:allPossibleCases'([A], B),
+    forall(
+        member('urn:example:is'(A, C), B),
+        'urn:example:is'(A, 'urn:example:observable') :+ 'urn:example:is'(A, C)
+    ).
+
+% query
+true :+ 'urn:example:is'(_, _).
